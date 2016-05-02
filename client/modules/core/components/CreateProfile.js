f9ca579eb3cc180b4eba65c51f066e0d4d19c656
Adrian Li <li.adrianmc@gmail.com>
2016-05-02 16:50:47 +0800

    added create profile page

---
+++ client/modules/core/components/CreateProfile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+
+class CreateProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>CreateProfile Page</div>;
+  }
+}
+
+const styles = {
+  container: {
+
+  },
+};
+
+export default CreateProfile;
